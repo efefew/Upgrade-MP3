89af6fd27cdc06a8d3e202ef1f0085f2c941bb02
Роман Сёмин <prodigy19913@mail.ru>
2024-02-26 00:26:08 +0300

    0.1

---
+++ UpgradeMP3/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+using HtmlAgilityPack;
+
+namespace UpgradeMP3
+{
+    internal class Program
+    {
+
+        #region Methods
+
+        private static void Main()
+        {
+            Mp3 mp3 = new Mp3();
+            mp3.SetImagesMP3();
+            Console.WriteLine($"готово");
+            _ = Console.ReadLine();
+        }
+
+        #endregion Methods
+    }
+
+    public class Mp3
+    {
+        #region Fields
+        private static readonly string APPLICATION_NAME = Assembly.GetEntryAssembly().GetName().Name;
+        private static readonly string APPLICATION_PATH = Assembly.GetEntryAssembly().Location;
+        private static readonly string PATH = APPLICATION_PATH.Remove(APPLICATION_PATH.Length - APPLICATION_NAME.Length - 4);//4 = количество букв в .mp3
+        private int count;
+        #endregion Fields
+
+        #region Methods
+
+        public void GetTags(string path)
+        {
+            //Создаем переменную с информацией о файле. В качестве параметра указываем полный путь.
+            using (TagLib.File audioFile = TagLib.File.Create(path + ".mp3"))
+            {
+                //Выводим нужную нам информацию на экран
+                Console.WriteLine("Альбом: {0}\nИсполнитель: {1}\nНазвание: {2}\nГод: {3}\nДлительность: {4}"
+                        , audioFile.Tag.Album
+                        , string.Join(", ", audioFile.Tag.Performers)
+                        , audioFile.Tag.Title
+                        , audioFile.Tag.Year
+                        , audioFile.Properties.Duration.ToString("mm\\:ss"));
+            }
+        }
+
+        public void AddMp3Tags(string path, string nameMP3/*, string urlImage*/, string songTitle, string[] artists, string album, uint trackNumber, string year)
+        {
+            TagLib.Id3v2.Tag.DefaultVersion = 3;
+            TagLib.Id3v2.Tag.ForceDefaultVersion = true;
+            TagLib.File file = TagLib.File.Create(path + nameMP3 + ".mp3");
+            //SetAlbumArt(urlImage, file);
+            file.Tag.Title = songTitle;
+            file.Tag.Performers = artists;
+            file.Tag.Album = album;
+            file.Tag.Track = trackNumber;
+            file.Tag.Year = (uint)Convert.ToInt32(Regex.Match(year, @"(\d)(\d)(\d)(\d)").Value);
+            file.RemoveTags(file.TagTypes & ~file.TagTypesOnDisk);
+            file.Save();
+        }
+
+        private void SetAlbumArt(string url, TagLib.File file)
+        {
+            byte[] imageBytes;
+            using (WebClient client = new WebClient())
+            {
+                imageBytes = client.DownloadData(url);
+            }
+
+            TagLib.Id3v2.AttachedPictureFrame cover = new TagLib.Id3v2.AttachedPictureFrame
+            {
+                Type = TagLib.PictureType.FrontCover,
+                Description = "",
+                MimeType = System.Net.Mime.MediaTypeNames.Image.Jpeg,
+                Data = imageBytes,
+                TextEncoding = TagLib.StringType.UTF16
+            };
+            file.Tag.Pictures = new TagLib.IPicture[] { cover };
+            Console.WriteLine($"url = {url}, file.Name = {file.Name}");
+        }
+        public void SetImagesMP3()
+        {
+            count = 0;
+            Console.ForegroundColor = ConsoleColor.White;
+            string pathDirectory = PATH;
+            if (Directory.Exists(pathDirectory))
+                SetImagesMP3(new DirectoryInfo(pathDirectory));
+        }
+        public void SetImagesMP3(DirectoryInfo directory)
+        {
+            if (directory.GetFiles().Length == 0)
+                return;
+
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                if (file.Name.EndsWith(".mp3"))
+                {
+                    SetImageMP3(file.Name, directory.FullName);
+                }
+            }
+
+            if (directory.GetDirectories().Length == 0)
+                return;
+
+            foreach (DirectoryInfo subDirectory in directory.GetDirectories())
+                SetImagesMP3(subDirectory);
+        }
+        public void SetImageMP3(string nameMP3, string path)
+        {
+            #region инициализация
+
+            string searchQuery = nameMP3 + " обложка"; // ваш поисковый запрос
+            string url = $"https://www.bing.com/images/search?q={searchQuery}";// URL для поискового запроса
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+            // Находим все ссылки на страницы с качесвенным фото на странице
+            HtmlNodeCollection imageNodes = doc.DocumentNode.SelectNodes("//a[contains(@class, 'iusc')]");
+            if (imageNodes == null)
+                return;
+            TagLib.File file = TagLib.File.Create(path + nameMP3);
+            TagLib.Id3v2.Tag.DefaultVersion = 3;
+            TagLib.Id3v2.Tag.ForceDefaultVersion = true;
+            string previewImageUrl, imageUrl;
+            HtmlDocument docFindImage;
+            HtmlNode findImageNode;
+
+            #endregion инициализация
+
+            // Проходим по всем найденным ссылкам на страницы с качесвенным фото
+            foreach (HtmlNode img in imageNodes)
+            {
+                previewImageUrl = img.GetAttributeValue("href", "");
+                if (string.IsNullOrEmpty(previewImageUrl) || !previewImageUrl.StartsWith(@"/images/search?"))
+                    continue;
+                previewImageUrl = @"https://www.bing.com" + previewImageUrl.Replace(';', '&') + '\n';
+                docFindImage = new HtmlWeb().Load(previewImageUrl);
+                findImageNode = docFindImage.DocumentNode.SelectSingleNode("//img");
+                if (findImageNode == null)
+                    continue;
+                imageUrl = findImageNode.GetAttributeValue("src", "").Replace(';', '&');
+                if (string.IsNullOrEmpty(imageUrl))
+                    continue;
+                try
+                {
+                    SetAlbumArt(imageUrl, file);
+                    file.Save();
+                    Console.Clear();
+                    Console.WriteLine($"{count} файлов обработано");
+                    count++;
+                    return;
+                }
+                catch { }
+            }
+        }
+
+        #endregion Methods
+    }
+}
